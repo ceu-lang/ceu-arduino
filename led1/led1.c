417ce37f66f74464b87a196430c0cab06f4d3552
Francisco Sant'Anna <francisco.santanna@gmail.com>
2014-01-24 01:30:03 -0200

    (^) led1: double link

--- led1/led1.c
+++ led1/led1.c
@@ -5,10 +5,12 @@ extern tceu_app app_led1;
 
 tceu_app* APPS;
 tceu_lnk* LNKS;
-tceu_lnk lnk = { &app_led1,1 ,  &drv_gpio,246 };
+tceu_lnk lnk1 = { &app_led1,1 ,  &drv_gpio,245, NULL };
+tceu_lnk lnk2 = { &app_led1,1 ,  &drv_gpio,246, NULL };
 
 void MAIN (void) {
     APPS = &drv_gpio;
     drv_gpio.nxt = &app_led1;
-    LNKS = &lnk;
+    LNKS = &lnk1;
+    lnk1.nxt = &lnk2;
 }

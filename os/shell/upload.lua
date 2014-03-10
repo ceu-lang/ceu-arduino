3ea281b53a6bde6e0b7ff1f6313ea4e0a777945a
Francisco Sant'Anna <francisco.santanna@gmail.com>
2014-03-10 15:19:42 -0300

    (^) upload (+) demos

--- os/shell/upload.lua
+++ os/shell/upload.lua
@@ -2,17 +2,24 @@
 
 local file = ...
 
+io.write('Uploading '..file..': ')
+io.flush()
+
 local i = 0
 for line in io.lines(file) do
     os.execute('/bin/echo -ne "'..line..'\n'..'" > /dev/ttyUSB0')
-    os.execute('/bin/echo -ne "'..line..'\n'..'"')
+    --print(line)
+    io.write('.')
+    io.flush()
+    os.execute('sleep 0.1')     -- TODO: shouldn't be required
     local n, tp = string.match(line, ':(..)....(..)')
     n = tonumber(n, 16)
     if tp == '00' then
         i = i + n
         if i == 256 then
-            os.execute('sleep 1')
+            os.execute('sleep 0.5')
             i = 0
         end
     end
 end
+print()

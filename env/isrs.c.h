765d43a5dcbcc3f8420ec9099c4e131808a10f69
fsantanna <francisco.santanna@gmail.com>
2016-11-15 16:31:01 -0200

    (>) isrs

--- env/isrs.c.h
+++ env/isrs.c.h
@@ -1,11 +1,17 @@
 #ifdef CEU_ISR__TIMER1_COMPA_vect_num
 ISR(TIMER1_COMPA_vect)
 {
-    //static int v = 0;
-    //digitalWrite(12, v=!v);
     if (isrs[TIMER1_COMPA_vect_num] != NULL) {
         isrs[TIMER1_COMPA_vect_num]();
     }
 }
 #endif
 
+#ifdef CEU_ISR__TIMER1_OVF_vect_num
+ISR(TIMER1_OVF_vect)
+{
+    if (isrs[TIMER1_OVF_vect_num] != NULL) {
+        isrs[TIMER1_OVF_vect_num]();
+    }
+}
+#endif

3caebb0cd8c94e5470feed45840104655622b2bb
Guilherme Simas <guilherme_abinader@hotmail.com>
2017-05-25 00:11:57 -0300

    Updated to comply with former code convention

--- env/isrs.c.h
+++ env/isrs.c.h
@@ -1,44 +1,45 @@
 #ifdef CEU_ISR__TIMER1_COMPA_vect_num
-ISR( TIMER1_COMPA_vect )
+ISR(TIMER1_COMPA_vect)
 {
-	if ( isrs[TIMER1_COMPA_vect_num].fun != NULL ) {
-		isrs[TIMER1_COMPA_vect_num].fun( isrs[TIMER1_OVF_vect_num].mem );
-	}
+....if (isrs[TIMER1_COMPA_vect_num].fun != NULL) {
+........isrs[TIMER1_COMPA_vect_num].fun(isrs[TIMER1_OVF_vect_num].mem);
+....}
 }
 #endif
 
 #ifdef CEU_ISR__TIMER1_OVF_vect_num
-ISR( TIMER1_OVF_vect )
+ISR(TIMER1_OVF_vect)
 {
-	if ( isrs[TIMER1_OVF_vect_num].fun != NULL ) {
-		isrs[TIMER1_OVF_vect_num].fun( isrs[TIMER1_OVF_vect_num].mem );
-	}
+....if (isrs[TIMER1_OVF_vect_num].fun != NULL) {
+........isrs[TIMER1_OVF_vect_num].fun(isrs[TIMER1_OVF_vect_num].mem);
+....}
 }
 #endif
 
 #ifdef CEU_ISR__USART_RX_vect_num
-ISR( USART_RX_vect )
+ISR(USART_RX_vect)
 {
-	if ( isrs[USART_RX_vect_num].fun != NULL ) {
-		isrs[USART_RX_vect_num].fun( isrs[USART_RX_vect_num].mem );
-	}
+....if (isrs[USART_RX_vect_num].fun != NULL) {
+........isrs[USART_RX_vect_num].fun(isrs[USART_RX_vect_num].mem);
+....}
 }
 #endif
 
 #ifdef CEU_ISR__USART_TX_vect_num
-ISR( USART_TX_vect )
+ISR(USART_TX_vect)
 {
-	if ( isrs[USART_TX_vect_num].fun != NULL ) {
-		isrs[USART_TX_vect_num].fun( isrs[USART_TX_vect_num].mem );
-	}
+....if (isrs[USART_TX_vect_num].fun != NULL) {
+........isrs[USART_TX_vect_num].fun(isrs[USART_TX_vect_num].mem);
+....}
 }
 #endif
 
 #ifdef CEU_ISR__ADC_vect_num
-ISR( ADC_vect )
+ISR(ADC_vect)
 {
-	if ( isrs[ADC_vect_num].fun != NULL ) {
-		isrs[ADC_vect_num].fun( isrs[ADC_vect_num].mem );
-	}
+....if (isrs[ADC_vect_num].fun != NULL) {
+........isrs[ADC_vect_num].fun(isrs[ADC_vect_num].mem);
+....}
 }
 #endif
+ 

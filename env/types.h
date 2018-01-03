7f167028f8939bfb884476bbb90f36bee6098fca
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-01-03 18:49:00 -0200

    (>) small fix to compile on ARM g++

--- env/types.h
+++ env/types.h
@@ -15,7 +15,9 @@ typedef int32_t  s32;
 typedef int64_t  s64;
 
 typedef uint8_t   u8;
-//typedef uint16_t u16;         // already defined in "USBAPI.h"
+#ifndef __AVR
+typedef uint16_t u16;         // already defined in "USBAPI.h"
+#endif
 typedef uint32_t u32;
 typedef uint64_t u64;
 
